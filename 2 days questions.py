2ee561b8f8e5727c664f38116cc1d584bd611570
B-Mikhail-V <bakulin.mv@gmail.com>
2021-12-01 23:46:33 +0300

    Task 3 finished

--- 2 days questions.py
+++ 2 days questions.py
@@ -2,20 +2,28 @@ import requests
 from datetime import datetime
 from datetime import timedelta
 from pprint import pprint
+import time
 
-question_list = []
-# today = datetime.date(datetime.today())
-today = int(datetime.today().timestamp())
-two_days_before = int((datetime.now() - timedelta(days=1)).timestamp())
-# upload_url = "https://api.stackexchange.com/2.3/questions?page=20&fromdate=1635724800&todate=1638230400&order=desc&sort=activity&tagged=Python&site=stackoverflow"
-upload_url = "https://api.stackexchange.com/2.3/questions?fromdate=" + str(two_days_before) + "&todate=" + str(today) + "&order=desc&sort=activity&tagged=Python&site=stackoverflow"
-response = requests.get(upload_url)
-for item in response.json()['items']:
-    question_list.append(item['title'])
-pprint(question_list)
-print(len(question_list))
 
-print(type(two_days_before))
-pprint(upload_url)
-pprint(two_days_before)
+class Stockover:
 
+    def __init__(self):
+        self.today = int(datetime.today().timestamp())
+        self.two_days_before = int((datetime.now() - timedelta(days=2)).timestamp())
+
+    def question_list(self, tag='Python'):
+        question_list = []
+        url_question = "https://api.stackexchange.com/2.3/questions"
+        params_const = {"page": "20", "order": "desc", "sort": "activity", "site": "stackoverflow", "tagged": tag}
+        params_var = {"fromdate": self.two_days_before, "todate": self.today}
+        params = {**params_var, **params_const}
+        response = requests.get(url_question, params=params)
+        for item in response.json()['items']:
+            question_list.append(item['title'])
+        return question_list
+
+
+if __name__ == '__main__':
+
+    sctk = Stockover()
+    pprint(sctk.question_list())
